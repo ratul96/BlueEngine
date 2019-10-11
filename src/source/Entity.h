d7945f2e6012a683a34745952a3bc984a568fe4c
Ratul Sarkar (s4915733) <s4915733@bournemouth.ac.uk>
2019-10-11 15:33:02 +0100

    nitialize commit

--- src/source/Entity.h
+++ src/source/Entity.h
@@ -1,1 +1,15 @@
 #pragma once
+#include "GL/glew.h"
+#include<GLM/glm.hpp>
+#include<SDL2/SDL.h>
+#include<string>
+class Entity
+{
+public:
+	Entity();
+	~Entity();
+	void updateEntity();
+protected:
+	std::vector<shared_ptr<Players>> players;
+
+};
