5b3ebdb155c7c1ad7545b76c0389d3281a64dca7
Ratul Sarkar <ratul96@yahoo.in>
2019-10-31 14:16:08 +0000

    initialize commit :wq

--- src/sandbox/TriangleRenderer.cpp
+++ src/sandbox/TriangleRenderer.cpp
@@ -1,5 +1,38 @@
 #include "TriangleRenderer.h"
 
+const GLfloat positions[25] = {
+  0.0f, 0.5f, 0.0f,
+  -0.5f, -0.5f, 0.0f,
+  0.5f, -0.5f, 0.0f
+};
+
+const GLfloat colors[25] = {
+  1.0f, 0.0f, 0.0f, 1.0f,
+  0.0f, 1.0f, 0.0f, 1.0f,
+  0.0f, 0.0f, 1.0f, 1.0f
+};
+const GLchar* vertexShaderSrc =
+"attribute vec3 in_Position;" \
+"attribute vec4 in_Color;" \
+"" \
+"varying vec4 ex_Color;" \
+"" \
+"void main()" \
+"{" \
+"  gl_Position = vec4(in_Position, 1.0);" \
+"  ex_Color = in_Color;" \
+"}" \
+"";
+
+const GLchar* fragmentShaderSrc =
+"varying vec4 ex_Color;" \
+"void main()" \
+"{" \
+"  gl_FragColor = ex_Color;" \
+"}" \
+"";
+
+
 
 
 TriangleRenderer::TriangleRenderer()
@@ -20,9 +53,10 @@ void TriangleRenderer::DrawTriangle()
 		throw std::exception();
 	}
 
-	SDL_Window *window = SDL_CreateWindow("Lab 4 - Architecture",
+	SDL_Window* window = SDL_CreateWindow("Lab 4 - Architecture",
 		SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
 		WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_RESIZABLE | SDL_WINDOW_OPENGL);
+
 	if (!SDL_GL_CreateContext(window))
 	{
 		throw std::exception();
@@ -61,16 +95,13 @@ void TriangleRenderer::DrawTriangle()
 		throw std::exception();
 	}
 
-
 	glBindBuffer(GL_ARRAY_BUFFER, colorsVboId);
-
 
 	// Upload a copy of the data from memory into the new VBO
 	glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW);
 
 	// Reset the state
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
-
 
 	GLuint vaoId = 0;
 
@@ -86,12 +117,12 @@ void TriangleRenderer::DrawTriangle()
 
 	// Bind the position VBO, assign it to position 0 on the bound VAO and flag it to be used
 	glBindBuffer(GL_ARRAY_BUFFER, positionsVboId);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (void *)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (void*)0);
 	glEnableVertexAttribArray(0);
 
 	// Bind the color VBO, assign it to position 1 on the bound VAO and flag it to be used
 	glBindBuffer(GL_ARRAY_BUFFER, colorsVboId);
-	glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 4 * sizeof(GLfloat), (void *)0);
+	glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 4 * sizeof(GLfloat), (void*)0);
 	glEnableVertexAttribArray(1);
 
 	// Reset the state
@@ -161,7 +192,7 @@ void TriangleRenderer::DrawTriangle()
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		glUseProgram(programId);
-
+		glBindVertexArray(vaoId);
 
 		glDrawArrays(GL_TRIANGLES, 0, 3);
 
--- src/sandbox/TriangleRenderer.h
+++ src/sandbox/TriangleRenderer.h
@@ -21,42 +21,9 @@ public:
 
 	SDL_Window *window;
 
-	const GLfloat positions[25] = {
-  0.0f, 0.5f, 0.0f,
-  -0.5f, -0.5f, 0.0f,
-  0.5f, -0.5f, 0.0f
-	};
-
-	const GLfloat colors[25] = {
-	  1.0f, 0.0f, 0.0f, 1.0f,
-	  0.0f, 1.0f, 0.0f, 1.0f,
-	  0.0f, 0.0f, 1.0f, 1.0f
-	};
-	const GLchar *vertexShaderSrc =
-		"attribute vec3 in_Position;" \
-		"attribute vec4 in_Color;" \
-		"" \
-		"varying vec4 ex_Color;" \
-		"" \
-		"void main()" \
-		"{" \
-		"  gl_Position = vec4(in_Position, 1.0);" \
-		"  ex_Color = in_Color;" \
-		"}" \
-		"";
-
-	const GLchar *fragmentShaderSrc =
-		"varying vec4 ex_Color;" \
-		"void main()" \
-		"{" \
-		"  gl_FragColor = ex_Color;" \
-		"}" \
-		"";
-
-
-
 	
-	friend class Component;
+	
+	
 
 
 };
