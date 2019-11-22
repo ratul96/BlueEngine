56e28e794f3d997956fa81e4933255b288a7cd0b
Ratul Sarkar (s4915733) <s4915733@bournemouth.ac.uk>
2019-11-22 11:07:18 +0000

    amending resources

--- src/source/Resources.h
+++ src/source/Resources.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "Resource.h"
+#include"Core.h"
 #include<memory>
 #include<vector>
 #include<iostream>
@@ -12,11 +13,18 @@ public:
 	std::shared_ptr<T>load(std::string path)
 	{
 		std::shared_ptr<T>re = std::make_shared<T>();
-		//resources.push_back(re);
 		re->core = core;
 		re->load(path);
 	}
+	template<typename T>
+	std::shared_ptr<T>create()
+	{
+		std::shared_ptr<T>re1 = std::make_shared<T>();
+		resources.push_back(re1);
+
+	}
 
 	std::vector<std::shared_ptr<Resource>>resources;
+	std::weak_ptr<Core>core;
 
 };
