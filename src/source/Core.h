404eb9249c6ed2a25fe68cfd87cc70a5f8e82c7a
Ratul Sarkar (s4915733) <s4915733@bournemouth.ac.uk>
2019-12-11 15:32:47 +0000

    UML updated

--- src/source/Core.h
+++ src/source/Core.h
@@ -21,7 +21,6 @@ public:
 	std::shared_ptr<Entity> addEntity();
 	std::vector<std::shared_ptr<Entity>>entities;
 	static std::shared_ptr<Core> initialize();
-	void initContext();
 	std::weak_ptr<Core> self;
 	std::shared_ptr<Resources>resources;
 	std::shared_ptr<Resources>getResources();
