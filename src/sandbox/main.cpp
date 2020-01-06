a65a9c13148ef55744f38e7b9acf9a5b7e888678
ratul96 <33395119+ratul96@users.noreply.github.com>
2020-01-06 00:51:59 +0000

    Using new shaders

--- src/sandbox/main.cpp
+++ src/sandbox/main.cpp
@@ -8,25 +8,40 @@ int main()
 {
 	std::shared_ptr<Core> core = Core::initialize();
 
-	std::shared_ptr<Entity> ce = core->addEntity();
-	std::shared_ptr<Entity>light = core->addEntity();
-	std::shared_ptr<Entity> entity = core->addEntity();
-	std::shared_ptr<Lighting>lighting = light->addComponent<Lighting>();
-	light->getComponent<Lighting>()->setLightPosition(1.2f, 1.0f,2.0f);
-	ce->getComponent<Transform>()->SetPosition(0, 0, 10);	
-	std::shared_ptr<Camera> camera = ce->addComponent<Camera>(); 
+	std::shared_ptr<Entity> ce = core->addEntity();    /*Add camera Entity
+													   *
+													   */
+
+	std::shared_ptr<Entity>light = core->addEntity();   /*Add Light Entity
+														*
+														*/
+	std::shared_ptr<Entity> entity = core->addEntity();   /*Add Object Entity
+														  *
+														  */
+	std::shared_ptr<Lighting>lighting = light->addComponent<Lighting>();  /*Add Lighting Component to Light Entity
+																		  *
+																		  */
+	ce->getComponent<Transform>()->SetPosition(0, 0, 10);	/* Setting position of camera
+															*
+															*/
+	std::shared_ptr<Camera> camera = ce->addComponent<Camera>(); /*Adding Camera component
+																 *
+																 */
 	entity->addComponent<Rotator>();
+	
 	
 	std::shared_ptr<Renderer> tr = entity->addComponent<Renderer>();
 	std::shared_ptr<Audio> audio = entity->addComponent<Audio>();
 	//call load function here
-	std::shared_ptr<NewSound> so = core->getResources()->load<NewSound>("../dixie_horn.ogg");
+	//std::shared_ptr<NewSound> so = core->getResources()->load<NewSound>("../dixie_horn.ogg");
 	std::shared_ptr<MeshComponent>me = core->getResources()->load<MeshComponent>("../curuthers.obj");
 	std::shared_ptr<Material>m = core->getResources()->loadTexture<Material>("../Whiskers.png");
+	std::shared_ptr<Shaders>sh = core->getResources()->load<Shaders>("../Shaders.txt");
 	// TODO
 	tr->setMesh(me);
 	tr->setMaterial(m);
-	audio->setSound(so);
+	tr->setShaders(sh);
+	//audio->setSound(so);
 
 	core->run();
 
