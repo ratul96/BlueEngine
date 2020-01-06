ad99d5507ab8f1bfd3eb056d4d7569600306d445
Karsten Pedersen <kpedersen@z420.local>
2020-01-06 14:48:49 +0000

    Placeholder for tweaks to delta time calculation.

--- src/source/Environment.cpp
+++ src/source/Environment.cpp
@@ -6,12 +6,23 @@ std::shared_ptr<Core>Environment::getCore()
 	return core.lock();
 }
 
+/*
+  void Environment::update()
+  {
+	unsigned int current = SDL_GetTicks();
+	deltaTs = (float)(current -(getCore()->lastTime)) / 1000.0f;    /**Calculating change in time between previous frame and current frame
+	getCore()->lastTime = current;
+  }
+*/
 float Environment::getDeltaTime()
 {
 	
 	
 	unsigned int current = SDL_GetTicks();
 	deltaTs = (float)(current -(getCore()->lastTime)) / 1000.0f;    /**Calculating change in time between previous frame and current frame
+	getCore()->lastTime = current;
 																	*/
 	return deltaTs;
+
+	// return this->deltaTs;
 }
